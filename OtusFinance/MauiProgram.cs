836ac41614cb23d47a1c75e25d191c0d4c2edb9a
Tyreek Brasfield <tyreekbrasfield@gmail.com>
2024-04-22 16:00:10 -0500

    Merged the first two pages and organized the code a bit

--- OtusFinance/MauiProgram.cs
+++ OtusFinance/MauiProgram.cs
@@ -14,7 +14,7 @@ namespace OtusFinance
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
                 });
-            builder.Services.AddTransient<MainPage>();
+            builder.Services.AddTransient<Pages.LandingPage>();
 
 #if DEBUG
     		builder.Logging.AddDebug();
